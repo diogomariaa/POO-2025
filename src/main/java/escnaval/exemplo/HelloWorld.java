b4d0cd41036d922a4adb47ba6c8cffd243405174
diogomariaa <diogoalvesmateus976@gmail.com>
2025-09-24 13:14:27 +0100

    Objetivo lab1: criar repositório Github

--- src/main/java/escnaval/exemplo/HelloWorld.java
+++ src/main/java/escnaval/exemplo/HelloWorld.java
@@ -1,1 +1,7 @@
- 
+package escnaval.exemplo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá, POO!! - Eu sou o <416 - Alves Mateus>");
+    }
+}
